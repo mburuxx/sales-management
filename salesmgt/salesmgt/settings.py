5f9a4d1fcd347b8207c534979a8bde7376c9dbb5
mburuxx <njihiamburu.3@gmail.com>
2025-10-06 06:29:53 +0300

    Add env

--- salesmgt/salesmgt/settings.py
+++ salesmgt/salesmgt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", default="django-insecure-r+-o3shs+q#&)$gw@cri@^*w^jb+&!kae4a_+8+!m@far)dmvk")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
